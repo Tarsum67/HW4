3a9a5283bc84cf37e4b00e45ed8482cfdc3db85f
Tarsum67 <taraum@icloud.com>
2023-10-17 22:24:05 -0500

    update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,5 @@
-var timerEl = document.querySelector(".timer-count");
+var timerEl = document.createElement("div");
+var head = document.querySelector("header")
 var startbtn = document.querySelector(".start-button");
 var questionsEl = document.querySelector("questions");
 var body = document.body;
@@ -31,6 +32,7 @@ var li16 = document.createElement("button");
 
 var next = document.createElement("button");
 var next1 = document.createElement("button");
+var done = document.createElement("button");
 question1.textContent = "What allows to create text with java script";
 question2.textContent = "what allows you to create a container with java script";
 question3.textContent = "what adds a element to the hmtl page in java script";
@@ -38,6 +40,7 @@ question4.textContent = "what changes the css in java script";
 
 next.textContent = "Next";
 next1.textContent = "Next";
+done.textContent = "DONE!";
 
 li1.textContent = "append";
 li2.textContent = "text-content";
@@ -49,10 +52,10 @@ li6.textContent = "set-attribute";
 li7.textContent = "append";
 li8.textContent = "create-elememt";
 
-li9.textContent = "a";
-li10.textContent = "b";
-li11.textContent = "c";
-li12.textContent = "d";
+li9.textContent = "create-elememt";
+li10.textContent = "set-attribute";
+li11.textContent = "append";
+li12.textContent = "text-content";
 
 li13.textContent = "a";
 li14.textContent = "b";
@@ -61,14 +64,15 @@ li16.textContent = "d";
 
 question1.setAttribute("style","font-size: 40px; margin: 5%; border: solid 2px black; padding: 5px; background-color: midnightblue; color: yellow; ");
 question2.setAttribute("style","font-size: 40px; margin: 5%; border: solid 2px black; padding: 5px; background-color: midnightblue; color: yellow; ");
+question3.setAttribute("style","font-size: 40px; margin: 5%; border: solid 2px black; padding: 5px; background-color: midnightblue; color: yellow; ");
 
 
-var timer;
 var timerCount;
 
 function startQuiz(){
     timerCount = 300;
     startbtn.disabled = true;
+    head.appendChild(timerEl)
     head.appendChild(question1);
     document.body.appendChild(listEl);
     listEl.appendChild(li1);
@@ -77,20 +81,20 @@ function startQuiz(){
     listEl.appendChild(li4);
     startTimer()
     startbtn.remove()
-   
     
 }
 
 function startTimer(){
-    timer = setInterval(function(){
+     var timer = setInterval(function() {
         timerCount--;
         timerEl.textContent = timerCount;
         
         if (timerCount === 0){
             clearInterval(timer);
             // outOfTime()
-            
-        }
+            timerEl.remove();
+           
+        };
         
     },1000);
 }
@@ -141,7 +145,25 @@ function startTimer(){
         listEl.appendChild(li12);
     };
 
-  
+    function correct2(){
+        alert("Correct!")
+        body.appendChild(done)
+    };
+    function wrong2(){  
+        alert("try agian")
+    };
+    function Done(){
+        timerEl.remove();
+        question3.remove();
+        li9.remove();
+        li10.remove();
+        li11.remove();
+        li12.remove();
+        done.remove();
+        questionsEl.remove();
+        
+      
+    };
         
         
         
@@ -156,11 +178,16 @@ function startTimer(){
         li7.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 35px;font-size: 30px;");
         li8.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 35px;font-size: 30px;");
 
+        li9.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 35px; font-size: 30px;");
+        li10.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 35px;font-size: 30px;");
+        li11.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 35px;font-size: 30px;");
+        li12.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 35px;font-size: 30px;");
+
         next.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 90%; margin-top: -2%; font-size: 30px; ");
         next1.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 90%; margin-top: -2%; font-size: 30px; ");
+        done.setAttribute("style", " color:yellow; background:midnightblue; padding: 5px; margin-left: 90%; margin-top: -2%; font-size: 30px; ");
 
-
-
+        timerEl.setAttribute("style"," display: flex; flex-wrap: wrap; justify-content: center; font-size: 50px; margin: 1%; position: relative; bottom: 10px; width: 10%;    border: 2px black;border-style: solid;padding: 2px;background-color: indigo;color:yellow;");
 
 
 
@@ -176,5 +203,11 @@ function startTimer(){
         li7.addEventListener("click",wrong1);
         li8.addEventListener("click",correct1);
         next1.addEventListener("click",Next1);
+
+        li9.addEventListener("click",wrong2);
+        li10.addEventListener("click",wrong2);
+        li11.addEventListener("click",correct2);
+        li12.addEventListener("click",wrong2);
+        done.addEventListener("click",Done);
         
         
